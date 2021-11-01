f5e983e809642aa8c593061983c5cc9b2d69ec89
Mona Lisa <santifranquez@gmail.com>
2021-11-01 17:38:24 -0300

    Se aregó validción de formulario y se mejoró el responsive

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 2500,
+      delay: 3000,
       disableOnInteraction: false,
     },
     pagination: {
@@ -28,3 +28,64 @@ function toggleMenu() {
 
 openMenuBtn.addEventListener('click', toggleMenu);
 closeMenuBtn.addEventListener('click', toggleMenu);
+
+
+// Scrollrevel
+
+ScrollReveal().reveal('.sobrenosotros');
+ScrollReveal().reveal('main');
+
+
+// Validación del formulario
+
+const form = document.querySelector('.formulario');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const nombre = document.querySelector('.nombre').value;
+  const apellido = document.querySelector('.apellido').value;
+  const email = document.querySelector('.email').value;
+
+  if(nombre === '' || apellido === '' || email === '') {
+    const div = document.querySelector('.alerta');
+    const p = document.createElement('P');
+    const error1 = document.querySelector('.error1');
+    
+    div.classList.add('advertencia', 'error1')
+
+    if(!error1) {
+
+      p.textContent = 'Debe rellenar todos los campos'
+  
+      div.appendChild(p);
+  
+      setTimeout(() => {
+        div.remove();
+      }, 2000);
+     
+    }
+
+
+  } else {
+    const div = document.querySelector('.alerta');
+    const p = document.createElement('P');
+    const error2 = document.querySelector('.error2');
+    div.classList.add('acierto', 'error2')
+  
+    if(!error2) {
+  
+        p.textContent = 'Gracias. Nos pondremos en contacto contigo a la brevedad'
+    
+        div.appendChild(p);
+    
+        setTimeout(() => {
+          div.remove();
+        }, 2000);
+    }
+    
+  
+    }
+  }
+  
+);
